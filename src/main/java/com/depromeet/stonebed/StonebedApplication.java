e75cd957ec665f1825760d0bd044a18712f01485
ybchar <uiurihappy@sju.ac.kr>
2024-06-18 21:13:19 +0900

    initial commit

---
+++ src/main/java/com/depromeet/stonebed/StonebedApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.stonebed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonebedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonebedApplication.class, args);
+	}
+
+}
