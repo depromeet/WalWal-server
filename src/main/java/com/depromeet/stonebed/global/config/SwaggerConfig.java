788e4bae7a0bf6b9aa7e19226dececec91c44cf5
Park Yun Chan <dbscks9793@gmail.com>
2024-06-25 13:21:47 +0900

    fix: Api path versioning 제거 및 SwaggerConfig 수정

--- src/main/java/com/depromeet/stonebed/global/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/stonebed/global/config/SwaggerConfig.java
@@ -1,13 +1,19 @@
 package com.depromeet.stonebed.global.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
+	private static final String packagesToScan = "com.depromeet.stonebed";
+
+	@Value("${api.version}")
+	private String apiVersion;
 
 	// OpenAPI Bean 설정
 	@Bean
@@ -15,17 +21,14 @@ public class SwaggerConfig {
 		return new OpenAPI()
 			.info(new Info()
 				.title("WalWal 프로젝트 API 문서화")
-				.version("1.0")
+				.version(apiVersion)
 				.description("WalWal 프로젝트의 Swagger UI 화면입니다."));
 	}
 
 	// GroupedOpenApi Bean 설정
 	@Bean
 	public GroupedOpenApi groupedOpenApi() {
-		String[] paths = {"/api/v1/**"};
-		String[] packagesToScan = {"com.depromeet.stonebed"};
 		return GroupedOpenApi.builder().group("WalWal API")
-			.pathsToMatch(paths)
 			.packagesToScan(packagesToScan)
 			.build();
 	}
