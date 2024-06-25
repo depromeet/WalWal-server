04ce58d5a623ba3db9a7c7a47ca1a8a746973d4b
ybchar <uiurihappy@sju.ac.kr>
2024-06-25 14:00:26 +0900

    fix: spotlessApply

--- src/main/java/com/depromeet/stonebed/StonebedApplication.java
+++ src/main/java/com/depromeet/stonebed/StonebedApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StonebedApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StonebedApplication.class, args);
     }
