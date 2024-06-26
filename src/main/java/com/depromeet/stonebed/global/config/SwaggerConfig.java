2c31cf2a022b576ad7d2d246edff8b73bc049155
yb__char <68099546+char-yb@users.noreply.github.com>
2024-06-26 22:41:49 +0900

    [WALWAL-104] Spotless 구성 (#19)
    * chore: pre-commit, spotless 구성
    
    * chore: pre-commit 실행 모드 추가
    
    * fix: spotlessApply
    
    * chore: googleJavaFormat aosp
    
    * chore: googleJavaFormat aosp
    
    * fix: spotlessApply
    
    * fix: spotlessApply version test
    
    * fix: spotlessApply

--- src/main/java/com/depromeet/stonebed/global/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/stonebed/global/config/SwaggerConfig.java
@@ -1,35 +1,36 @@
 package com.depromeet.stonebed.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 @Configuration
 public class SwaggerConfig {
-	private static final String PACKAGES_TO_SCAN = "com.depromeet.stonebed";
+    private static final String PACKAGES_TO_SCAN = "com.depromeet.stonebed";
 
-	@Value("${api.version}")
-	private String apiVersion;
+    @Value("${api.version}")
+    private String apiVersion;
 
-	// OpenAPI Bean 설정
-	@Bean
-	public OpenAPI openAPI() {
-		return new OpenAPI()
-			.info(new Info()
-				.title("WalWal 프로젝트 API 문서화")
-				.version(apiVersion)
-				.description("WalWal 프로젝트의 Swagger UI 화면입니다."));
-	}
+    // OpenAPI Bean 설정
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("WalWal 프로젝트 API 문서화")
+                                .version(apiVersion)
+                                .description("WalWal 프로젝트의 Swagger UI 화면입니다."));
+    }
 
-	// GroupedOpenApi Bean 설정
-	@Bean
-	public GroupedOpenApi groupedOpenApi() {
-		return GroupedOpenApi.builder().group("WalWal API")
-			.packagesToScan(PACKAGES_TO_SCAN)
-			.build();
-	}
+    // GroupedOpenApi Bean 설정
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("WalWal API")
+                .packagesToScan(PACKAGES_TO_SCAN)
+                .build();
+    }
 }
