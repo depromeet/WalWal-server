db6eb30229767ee1ea7f1008b7a1a494191ec850
ybchar <uiurihappy@sju.ac.kr>
2024-06-27 15:35:31 +0900

    fix: spotlessApply

--- src/main/java/com/depromeet/stonebed/domain/common/BaseTimeEntity.java
+++ src/main/java/com/depromeet/stonebed/domain/common/BaseTimeEntity.java
@@ -1,15 +1,13 @@
 package com.depromeet.stonebed.domain.common;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
