4700bb2abb9ceb0d33f1d691bd9a344bcf0315a8
ybchar <uiurihappy@sju.ac.kr>
2024-06-27 14:20:06 +0900

    fix: spotlessApply

--- src/test/java/com/depromeet/stonebed/TestQuerydslConfig.java
+++ src/test/java/com/depromeet/stonebed/TestQuerydslConfig.java
@@ -1,13 +1,11 @@
 package com.depromeet.stonebed;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class TestQuerydslConfig {
