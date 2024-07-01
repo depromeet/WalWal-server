73109c5f54f862beb3e597b514fb0c5cd80cea1b
yb__char <68099546+char-yb@users.noreply.github.com>
2024-07-01 23:13:42 +0900

    [WALWAL-114] record -> class 변경 (#30)
    * fix: record -> class 변경
    
    * fix: profileImageUrl getter 삭제
    
    * feat: Schema Swagger 프로퍼티

--- src/main/java/com/depromeet/stonebed/domain/member/domain/Profile.java
+++ src/main/java/com/depromeet/stonebed/domain/member/domain/Profile.java
@@ -2,12 +2,29 @@ package com.depromeet.stonebed.domain.member.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-public record Profile(
-        @Schema(description = "닉네임", example = "왈왈멍") String nickname,
-        @Schema(description = "프로필 이미지 URL", example = "./profile.jpg") String profileImageUrl) {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Profile {
+
+    @Schema(description = "닉네임", example = "왈왈멍")
+    private String nickname;
+
+    @Schema(description = "프로필 이미지 URL", example = "./profile.jpg")
+    private String profileImageUrl;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Profile(String nickname, String profileImageUrl) {
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+
     public static Profile createProfile(String nickname, String profileImageUrl) {
-        return new Profile(nickname, profileImageUrl);
+        return Profile.builder().nickname(nickname).profileImageUrl(profileImageUrl).build();
     }
 }
