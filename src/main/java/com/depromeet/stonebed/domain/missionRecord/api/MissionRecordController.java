ca1722167b32bebfaefae6e2cbb225029ad130b2
Park Yun Chan <dbscks9793@gmail.com>
2024-07-12 13:31:14 +0900

    fix: @Tag어노테이션 추가 및 컨트롤러반환값 수정

--- src/main/java/com/depromeet/stonebed/domain/missionRecord/api/MissionRecordController.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/api/MissionRecordController.java
@@ -3,11 +3,10 @@ package com.depromeet.stonebed.domain.missionRecord.api;
 import com.depromeet.stonebed.domain.missionRecord.application.MissionRecordService;
 import com.depromeet.stonebed.domain.missionRecord.dto.request.MissionRecordCreateRequest;
 import com.depromeet.stonebed.domain.missionRecord.dto.response.MissionRecordCreateResponse;
-import com.depromeet.stonebed.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "미션 기록", description = "미션 기록 관련 API입니다.")
 @RestController
 @RequestMapping("/records")
 @RequiredArgsConstructor
@@ -24,15 +24,14 @@ public class MissionRecordController {
 
     @Operation(summary = "미션 기록 저장", description = "미션 완료 후 기록을 저장한다.")
     @PostMapping
-    public ApiResponse completeMission(@Valid @RequestBody MissionRecordCreateRequest request) {
-        MissionRecordCreateResponse response = missionRecordService.completeMission(request);
-        return ApiResponse.success(HttpStatus.CREATED.value(), response);
+    public MissionRecordCreateResponse completeMission(
+            @Valid @RequestBody MissionRecordCreateRequest request) {
+        return missionRecordService.completeMission(request);
     }
 
     @Operation(summary = "미션 기록 삭제", description = "미션 기록을 삭제한다.")
     @DeleteMapping("/{recordId}")
-    public ApiResponse deleteMissionRecord(@PathVariable Long recordId) {
+    public void deleteMissionRecord(@PathVariable Long recordId) {
         missionRecordService.deleteMissionRecord(recordId);
-        return ApiResponse.success(HttpStatus.NO_CONTENT.value(), null);
     }
 }
