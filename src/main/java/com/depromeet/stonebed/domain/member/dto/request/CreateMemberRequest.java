5bdf62136b35837ead1a68b39171845ed8a602ac
yb__char <68099546+char-yb@users.noreply.github.com>
2024-07-30 10:25:19 +0900

    [WALWAL-152] register Request Body 불필요한 필드 삭제 (#62)
    * fix: register Request Body
    
    * fix: 마케팅 수신 동의여부 삭제

--- src/main/java/com/depromeet/stonebed/domain/member/dto/request/CreateMemberRequest.java
+++ src/main/java/com/depromeet/stonebed/domain/member/dto/request/CreateMemberRequest.java
@@ -1,11 +1,5 @@
 package com.depromeet.stonebed.domain.member.dto.request;
 
-import com.depromeet.stonebed.domain.member.domain.MarketingAgreement;
 import com.depromeet.stonebed.domain.member.domain.RaisePet;
 
-public record CreateMemberRequest(
-        String nickname,
-        String profileImageUrl,
-        RaisePet raisePet,
-        String email,
-        MarketingAgreement marketingAgreement) {}
+public record CreateMemberRequest(String nickname, RaisePet raisePet) {}
