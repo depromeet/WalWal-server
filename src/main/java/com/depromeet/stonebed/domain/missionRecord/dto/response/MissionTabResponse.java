206ed9f740bab9ee34d4175c5e43dbf5f6cdea40
Park Yun Chan <dbscks9793@gmail.com>
2024-07-31 16:21:31 +0900

    fix: 안쓰는 Response 및 스키마 수정

--- src/main/java/com/depromeet/stonebed/domain/missionRecord/dto/response/MissionTabResponse.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/dto/response/MissionTabResponse.java
@@ -4,7 +4,7 @@ import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MissionTabResponse(
-        @Schema(description = "미션 상태", example = "IN_PROGRESS") MissionRecordStatus status) {
+        @Schema(description = "미션 상태", example = "NOT_COMPLETED") MissionRecordStatus status) {
     public static MissionTabResponse from(MissionRecordStatus status) {
         return new MissionTabResponse(status);
     }
