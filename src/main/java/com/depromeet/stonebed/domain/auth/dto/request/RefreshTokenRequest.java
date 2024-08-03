d37119e98a73c18fbdafae669bc536f0bb049ac5
ybchar <uiurihappy@sju.ac.kr>
2024-08-03 18:29:31 +0900

    fix: 임시 토큰 여부 판별

--- src/main/java/com/depromeet/stonebed/domain/auth/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/depromeet/stonebed/domain/auth/dto/request/RefreshTokenRequest.java
@@ -1,3 +1,6 @@
 package com.depromeet.stonebed.domain.auth.dto.request;
 
-public record RefreshTokenRequest(String refreshToken) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record RefreshTokenRequest(
+        @Schema(description = "리프레시 토큰", example = "refreshToken") String refreshToken) {}
