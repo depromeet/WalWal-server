d5ea75621c2c6b0c22bb3aa7cc5b206a40480e89
Park Yun Chan <dbscks9793@gmail.com>
2024-08-07 21:40:30 +0900

    fix: @EnableScheduling 어노테이션 추가

--- src/main/java/com/depromeet/stonebed/StonebedApplication.java
+++ src/main/java/com/depromeet/stonebed/StonebedApplication.java
@@ -2,7 +2,9 @@ package com.depromeet.stonebed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class StonebedApplication {
     public static void main(String[] args) {
