effe0a5604b33a8544a5b7cd7000d64dda6d0a96
Park Yun Chan <dbscks9793@gmail.com>
2024-08-08 16:49:34 +0900

    fix: env 변수 수정

--- src/main/java/com/depromeet/stonebed/global/config/fcm/FcmConfig.java
+++ src/main/java/com/depromeet/stonebed/global/config/fcm/FcmConfig.java
@@ -4,24 +4,27 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FcmConfig {
 
-    @Value("${FCM_CREDENTIAL}")
-    private String firebaseCredential;
-
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
+        Dotenv dotenv = Dotenv.load();
+        String firebaseCredential = dotenv.get("FCM_CREDENTIAL");
+
+        if (firebaseCredential == null || firebaseCredential.isEmpty()) {
+            throw new IllegalStateException("FCM_CREDENTIAL environment variable is not set.");
+        }
+
         ByteArrayInputStream credentials =
                 new ByteArrayInputStream(firebaseCredential.getBytes(StandardCharsets.UTF_8));
-
         FirebaseOptions options =
                 FirebaseOptions.builder()
                         .setCredentials(GoogleCredentials.fromStream(credentials))
