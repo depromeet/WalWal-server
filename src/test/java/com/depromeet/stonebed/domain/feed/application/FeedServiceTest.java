93d1c6e73dfe3f2d65ada40e28a8ba61dbbf83e6
nohkwanok <cloq@kakao.com>
2024-08-15 02:40:24 +0900

    test: 테스트 코드 추가

--- src/test/java/com/depromeet/stonebed/domain/feed/application/FeedServiceTest.java
+++ src/test/java/com/depromeet/stonebed/domain/feed/application/FeedServiceTest.java
@@ -2,6 +2,7 @@ package com.depromeet.stonebed.domain.feed.application;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 import com.depromeet.stonebed.FixtureMonkeySetUp;
 import com.depromeet.stonebed.domain.feed.dao.FeedRepository;
@@ -10,10 +11,14 @@ import com.depromeet.stonebed.domain.feed.dto.request.FeedGetRequest;
 import com.depromeet.stonebed.domain.feed.dto.response.FeedGetResponse;
 import com.depromeet.stonebed.domain.member.domain.Member;
 import com.depromeet.stonebed.domain.mission.domain.Mission;
+import com.depromeet.stonebed.domain.missionRecord.dao.MissionRecordBoostRepository;
+import com.depromeet.stonebed.domain.missionRecord.dao.MissionRecordRepository;
 import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecord;
+import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordBoost;
 import com.depromeet.stonebed.global.util.MemberUtil;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,6 +33,8 @@ class FeedServiceTest extends FixtureMonkeySetUp {
     @InjectMocks private FeedService feedService;
 
     @Mock private FeedRepository feedRepository;
+    @Mock private MissionRecordRepository missionRecordRepository;
+    @Mock private MissionRecordBoostRepository missionRecordBoostRepository;
     @Mock private MemberUtil memberUtil;
 
     @Test
@@ -120,5 +127,25 @@ class FeedServiceTest extends FixtureMonkeySetUp {
         verify(memberUtil).getCurrentMember();
         verify(feedRepository)
                 .getFeedContentsUsingCursor(Long.parseLong(cursor), member.getId(), 5);
+    }
+
+    @Test
+    void 부스트_성공() {
+        // Given
+        Member member = fixtureMonkey.giveMeOne(Member.class);
+        MissionRecord missionRecord = fixtureMonkey.giveMeOne(MissionRecord.class);
+
+        when(memberUtil.getCurrentMember()).thenReturn(member);
+        when(missionRecordRepository.findById(missionRecord.getId()))
+                .thenReturn(Optional.of(missionRecord));
+        when(missionRecordBoostRepository.save(any(MissionRecordBoost.class))).thenReturn(null);
+
+        // When
+        feedService.createBoost(missionRecord.getId(), 10L);
+
+        // Then
+        verify(memberUtil).getCurrentMember();
+        verify(missionRecordRepository).findById(missionRecord.getId());
+        verify(missionRecordBoostRepository).save(any(MissionRecordBoost.class));
     }
 }
