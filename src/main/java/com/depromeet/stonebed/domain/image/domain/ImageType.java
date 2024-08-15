28dd68c3de3bb29a1a9c7297c5d09ecab9a12168
Park Yun Chan <dbscks9793@gmail.com>
2024-08-15 14:54:23 +0900

    feature: 미션 이미지 업로드 구현

--- src/main/java/com/depromeet/stonebed/domain/image/domain/ImageType.java
+++ src/main/java/com/depromeet/stonebed/domain/image/domain/ImageType.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ImageType {
+    MISSION("mission"),
     MISSION_RECORD("mission_record"),
     MEMBER_PROFILE("member_profile"),
     ;
