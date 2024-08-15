b87f0fedfc9423bb2e3bc535a238f3a7e606200c
Park Yun Chan <dbscks9793@gmail.com>
2024-08-15 14:01:48 +0900

    fix: MissionRecordSaveRequest 스키마 수정

--- src/main/java/com/depromeet/stonebed/domain/missionRecord/dto/request/MissionRecordSaveRequest.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/dto/request/MissionRecordSaveRequest.java
@@ -5,4 +5,4 @@ import jakarta.validation.constraints.NotNull;
 
 public record MissionRecordSaveRequest(
         @NotNull @Schema(description = "미션 ID", example = "1") Long missionId,
-        @Schema(description = "미션 소감", example = "너무 귀엽다...") String text) {}
+        @Schema(description = "미션 소감", example = "너무 귀엽다...") String content) {}
