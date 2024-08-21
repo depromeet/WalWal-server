94564adaec4b215b46f5ed6751bad466c9fd4699
nohkwanok <cloq@kakao.com>
2024-08-22 02:13:27 +0900

    fix: 테스트 상수 전역으로

--- src/test/java/com/depromeet/stonebed/domain/feed/application/FeedServiceTest.java
+++ src/test/java/com/depromeet/stonebed/domain/feed/application/FeedServiceTest.java
@@ -26,6 +26,10 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class FeedServiceTest extends FixtureMonkeySetUp {
+    int DEFAULT_LIMIT = 5;
+    Long DEFAULT_TOTAL_BOOST_COUNT = 100L;
+    String DEFAULT_CURSOR = "5";
+    String INVALID_CURSOR = "2024-08-01";
 
     @InjectMocks private FeedService feedService;
 
@@ -42,16 +46,18 @@ class FeedServiceTest extends FixtureMonkeySetUp {
                             fixtureMonkey.giveMeOne(Mission.class),
                             fixtureMonkey.giveMeOne(MissionRecord.class),
                             fixtureMonkey.giveMeOne(Member.class),
-                            100L));
+                            DEFAULT_TOTAL_BOOST_COUNT));
         }
 
-        when(feedRepository.getFeedContentsUsingCursor(null, null, 5)).thenReturn(feeds);
+        when(feedRepository.getFeedContentsUsingCursor(null, null, DEFAULT_LIMIT))
+                .thenReturn(feeds);
         when(feedRepository.getNextFeedContent(
                         feeds.get(feeds.size() - 1).missionRecord().getId(), null))
                 .thenReturn(null);
 
         // When
-        FeedGetResponse feedGetResponse = feedService.getFeed(new FeedGetRequest(null, null, 5));
+        FeedGetResponse feedGetResponse =
+                feedService.getFeed(new FeedGetRequest(null, null, DEFAULT_LIMIT));
 
         // Then
         assertThat(feedGetResponse.list().size()).isEqualTo(5);
@@ -62,7 +68,6 @@ class FeedServiceTest extends FixtureMonkeySetUp {
     void 피드_조회_커서_사용_성공() {
         // Given
         List<FindFeedDto> feeds = new ArrayList<>();
-        String cursor = "3";
 
         for (int i = 0; i < 5; i++) {
             feeds.add(
@@ -70,28 +75,38 @@ class FeedServiceTest extends FixtureMonkeySetUp {
                             fixtureMonkey.giveMeOne(Mission.class),
                             fixtureMonkey.giveMeOne(MissionRecord.class),
                             fixtureMonkey.giveMeOne(Member.class),
-                            100L));
+                            DEFAULT_TOTAL_BOOST_COUNT));
         }
 
-        when(feedRepository.getFeedContentsUsingCursor(Long.parseLong(cursor), null, 5))
+        when(feedRepository.getFeedContentsUsingCursor(
+                        Long.parseLong(DEFAULT_CURSOR), null, DEFAULT_LIMIT))
                 .thenReturn(feeds);
+
+        FindFeedDto nextFeed =
+                FindFeedDto.from(
+                        fixtureMonkey.giveMeOne(Mission.class),
+                        fixtureMonkey.giveMeOne(MissionRecord.class),
+                        fixtureMonkey.giveMeOne(Member.class),
+                        DEFAULT_TOTAL_BOOST_COUNT);
+
         when(feedRepository.getNextFeedContent(
                         feeds.get(feeds.size() - 1).missionRecord().getId(), null))
-                .thenReturn(null);
+                .thenReturn(nextFeed);
 
         // When
-        FeedGetResponse feedGetResponse = feedService.getFeed(new FeedGetRequest(cursor, null, 5));
+        FeedGetResponse feedGetResponse =
+                feedService.getFeed(new FeedGetRequest(DEFAULT_CURSOR, null, DEFAULT_LIMIT));
 
         // Then
         assertThat(feedGetResponse.list().size()).isEqualTo(5);
-        assertThat(feedGetResponse.nextCursor()).isEqualTo(null);
+        assertThat(feedGetResponse.nextCursor())
+                .isEqualTo(String.valueOf(feeds.get(feeds.size() - 1).missionRecord().getId()));
     }
 
     @Test
     void 피드_조회_커서_사용_마지막_성공() {
         // Given
         List<FindFeedDto> feeds = new ArrayList<>();
-        String cursor = "5";
 
         for (int i = 0; i < 3; i++) {
             feeds.add(
@@ -99,24 +114,19 @@ class FeedServiceTest extends FixtureMonkeySetUp {
                             fixtureMonkey.giveMeOne(Mission.class),
                             fixtureMonkey.giveMeOne(MissionRecord.class),
                             fixtureMonkey.giveMeOne(Member.class),
-                            100L));
+                            DEFAULT_TOTAL_BOOST_COUNT));
         }
 
-        FindFeedDto nextFeed =
-                FindFeedDto.from(
-                        fixtureMonkey.giveMeOne(Mission.class),
-                        fixtureMonkey.giveMeOne(MissionRecord.class),
-                        fixtureMonkey.giveMeOne(Member.class),
-                        100L);
-
-        when(feedRepository.getFeedContentsUsingCursor(Long.parseLong(cursor), null, 5))
+        when(feedRepository.getFeedContentsUsingCursor(
+                        Long.parseLong(DEFAULT_CURSOR), null, DEFAULT_LIMIT))
                 .thenReturn(feeds);
         when(feedRepository.getNextFeedContent(
                         feeds.get(feeds.size() - 1).missionRecord().getId(), null))
-                .thenReturn(nextFeed);
+                .thenReturn(null);
 
         // When
-        FeedGetResponse feedGetResponse = feedService.getFeed(new FeedGetRequest(cursor, null, 5));
+        FeedGetResponse feedGetResponse =
+                feedService.getFeed(new FeedGetRequest(DEFAULT_CURSOR, null, DEFAULT_LIMIT));
 
         // Then
         assertThat(feedGetResponse.list().size()).isEqualTo(3);
@@ -125,14 +135,13 @@ class FeedServiceTest extends FixtureMonkeySetUp {
 
     @Test
     void 피드_조회_유효하지_않은_커서_실패() {
-        // Given
-        String cursor = "2024-08-01";
-
-        // When
+        // Given & When
         CustomException exception =
                 assertThrows(
                         CustomException.class,
-                        () -> feedService.getFeed(new FeedGetRequest(cursor, null, 5)));
+                        () ->
+                                feedService.getFeed(
+                                        new FeedGetRequest(INVALID_CURSOR, null, DEFAULT_LIMIT)));
 
         // Then: 에러코드 검증
         assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.INVALID_CURSOR_FORMAT);
