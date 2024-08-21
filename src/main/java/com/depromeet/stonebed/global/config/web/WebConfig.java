62c020775413fa874d2f83b1929bc2f396d85a6d
ybchar <uiurihappy@sju.ac.kr>
2024-08-22 02:44:21 +0900

    chore: webMvc cors 제거

--- src/main/java/com/depromeet/stonebed/global/config/web/WebConfig.java
+++ src/main/java/com/depromeet/stonebed/global/config/web/WebConfig.java
@@ -13,9 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 
     @Override
