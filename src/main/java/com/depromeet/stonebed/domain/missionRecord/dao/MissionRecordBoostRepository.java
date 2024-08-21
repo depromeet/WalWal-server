35464a9758d5f7d9fdc9aba161a086c2d2ca30f9
yb__char <68099546+char-yb@users.noreply.github.com>
2024-08-21 05:56:05 -0700

    fix: 회원탈퇴 데이터 초기화 (#170)

--- src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordBoostRepository.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordBoostRepository.java
@@ -2,6 +2,7 @@ package com.depromeet.stonebed.domain.missionRecord.dao;
 
 import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordBoost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,4 +10,9 @@ public interface MissionRecordBoostRepository extends JpaRepository<MissionRecor
     @Query(
             "SELECT SUM(mrb.count) FROM MissionRecordBoost mrb WHERE mrb.missionRecord.id = :missionRecordId")
     Long sumBoostCountByMissionRecord(@Param("missionRecordId") Long missionRecordId);
+
+    @Modifying
+    @Query(
+            "UPDATE MissionRecordBoost mrb SET mrb.deletedAt = CURRENT_TIMESTAMP WHERE mrb.member.id = :memberId")
+    void deleteAllByMember(@Param("memberId") Long memberId);
 }
--- src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordRepository.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordRepository.java
@@ -7,6 +7,9 @@ import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordStatus;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRecordRepository
         extends JpaRepository<MissionRecord, Long>, MissionRecordRepositoryCustom {
@@ -18,4 +21,9 @@ public interface MissionRecordRepository
     List<MissionRecord> findAllByStatus(MissionRecordStatus status);
 
     List<MissionRecord> findByIdIn(List<Long> ids);
+
+    @Modifying
+    @Query(
+            "UPDATE MissionRecord mr SET mr.deletedAt = CURRENT_TIMESTAMP WHERE mr.member.id = :memberId")
+    void deleteAllByMember(@Param("memberId") Long memberId);
 }
