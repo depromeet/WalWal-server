fbde22935e40120f5ae3c0c58de6ca0774069811
Park Yun Chan <dbscks9793@gmail.com>
2024-08-21 23:58:43 +0900

    fix: test코드 수정

--- src/test/java/com/depromeet/stonebed/domain/fcm/application/FcmScheduledServiceTest.java
+++ src/test/java/com/depromeet/stonebed/domain/fcm/application/FcmScheduledServiceTest.java
@@ -12,6 +12,7 @@ import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -27,6 +28,7 @@ public class FcmScheduledServiceTest extends FixtureMonkeySetUp {
     @Mock private FcmService fcmService;
     @Mock private FcmNotificationService fcmNotificationService;
     @Mock private FcmRepository fcmRepository;
+    @Mock private FcmTokenService fcmTokenService;
     @Mock private MissionRecordRepository missionRecordRepository;
 
     @InjectMocks private FcmScheduledService fcmScheduledService;
@@ -52,42 +54,46 @@ public class FcmScheduledServiceTest extends FixtureMonkeySetUp {
     }
 
     @Test
-    void 매일_정기_알림을_모든_사용자에게_전송한다() {
+    void 매일_정기_알림을_모든_사용자에게_전송하고_저장한다() {
+        // given
+        List<String> tokens = fixtureMonkey.giveMe(String.class, 5);
+        when(fcmTokenService.getAllTokens()).thenReturn(tokens);
+
         // when
         fcmScheduledService.sendDailyNotification();
 
         // then
-        verify(fcmService, times(1)).sendMulticastMessageToAll(any());
         verify(fcmNotificationService, times(1))
-                .saveNotification(
-                        any(), eq("미션 시작!"), eq("새로운 미션을 지금 시작해보세요!"), isNull(), eq(false));
+                .sendAndSaveNotifications(eq("미션 시작!"), eq("새로운 미션을 지금 시작해보세요!"), eq(tokens));
     }
 
     @Test
-    void 미완료_미션_사용자에게_리마인더를_전송한다() {
+    void 미완료_미션_사용자에게_리마인더를_전송하고_저장한다() {
         // given
         List<MissionRecord> missionRecords = fixtureMonkey.giveMe(MissionRecord.class, 2);
         when(missionRecordRepository.findAllByStatus(MissionRecordStatus.NOT_COMPLETED))
                 .thenReturn(missionRecords);
 
-        missionRecords.forEach(
-                missionRecord -> {
-                    FcmToken token =
-                            fixtureMonkey
-                                    .giveMeBuilder(FcmToken.class)
-                                    .set("member", missionRecord.getMember())
-                                    .sample();
-                    when(fcmRepository.findByMember(missionRecord.getMember()))
-                            .thenReturn(Optional.of(token));
-                });
+        List<String> tokens =
+                missionRecords.stream()
+                        .map(
+                                missionRecord -> {
+                                    FcmToken token =
+                                            fixtureMonkey
+                                                    .giveMeBuilder(FcmToken.class)
+                                                    .set("member", missionRecord.getMember())
+                                                    .sample();
+                                    when(fcmRepository.findByMember(missionRecord.getMember()))
+                                            .thenReturn(Optional.of(token));
+                                    return token.getToken();
+                                })
+                        .collect(Collectors.toList());
 
         // when
         fcmScheduledService.sendReminderToIncompleteMissions();
 
         // then
-        verify(fcmService, times(1)).sendMulticastMessage(any(), anyList());
         verify(fcmNotificationService, times(1))
-                .saveNotification(
-                        any(), eq("미션 리마인드"), eq("미션 종료까지 5시간 남았어요!"), isNull(), eq(false));
+                .sendAndSaveNotifications(eq("미션 리마인드"), eq("미션 종료까지 5시간 남았어요!"), eq(tokens));
     }
 }
