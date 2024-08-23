194fc45b87ce54f64990e56ec2d01af6c2aa3e36
Park Yun Chan <dbscks9793@gmail.com>
2024-08-23 14:51:50 +0900

    fix: where메서드 분리 후 BooleanExpression사용

--- src/main/java/com/depromeet/stonebed/domain/fcm/dao/FcmRepositoryImpl.java
+++ src/main/java/com/depromeet/stonebed/domain/fcm/dao/FcmRepositoryImpl.java
@@ -4,6 +4,7 @@ import static com.depromeet.stonebed.domain.fcm.domain.QFcmToken.*;
 import static com.depromeet.stonebed.domain.member.domain.QMember.*;
 
 import com.depromeet.stonebed.domain.member.domain.MemberStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,15 +12,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FcmRepositoryImpl implements FcmRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public List<String> findAllValidTokens() {
-        return queryFactory
+        return jpaQueryFactory
                 .select(fcmToken.token)
                 .from(fcmToken)
                 .join(fcmToken.member, member)
-                .where(member.status.eq(MemberStatus.NORMAL).and(fcmToken.token.isNotNull()))
+                .where(isMemberStatusNormal(), isTokenNotNull())
                 .fetch();
+    }
+
+    private BooleanExpression isMemberStatusNormal() {
+        return member.status.eq(MemberStatus.NORMAL);
+    }
+
+    private BooleanExpression isTokenNotNull() {
+        return fcmToken.token.isNotNull();
     }
 }
