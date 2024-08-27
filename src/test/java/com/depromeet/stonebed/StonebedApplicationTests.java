7b09fe45360434c8cc8fc720c1f88c34f4612c3e
Park Yun Chan <dbscks9793@gmail.com>
2024-08-27 21:48:25 +0900

    fix: test코드 SqsAutoConfiguration Exclude처리

--- src/test/java/com/depromeet/stonebed/StonebedApplicationTests.java
+++ src/test/java/com/depromeet/stonebed/StonebedApplicationTests.java
@@ -1,17 +1,24 @@
 package com.depromeet.stonebed;
 
 import com.depromeet.stonebed.domain.fcm.application.FcmNotificationService;
+import com.depromeet.stonebed.domain.sqs.application.SqsMessageService;
 import com.google.firebase.messaging.FirebaseMessaging;
+import io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
+@EnableAutoConfiguration(exclude = {SqsAutoConfiguration.class})
 @SpringBootTest
 @ActiveProfiles("test")
 class StonebedApplicationTests {
     @MockBean private FirebaseMessaging firebaseMessaging;
     @MockBean private FcmNotificationService fcmNotificationService;
+    @MockBean private SqsMessageService sqsMessageService;
+    @MockBean private SqsClient sqsClient;
 
     @Test
     void contextLoads() {}
