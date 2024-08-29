ce7ac7b02bd678ac039bedba8be22d5a7bf5cb83
ybchar <uiurihappy@sju.ac.kr>
2024-08-29 18:31:59 +0900

    fix: 어노테이션 import

--- src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordBoostRepository.java
+++ src/main/java/com/depromeet/stonebed/domain/missionRecord/dao/MissionRecordBoostRepository.java
@@ -3,6 +3,7 @@ package com.depromeet.stonebed.domain.missionRecord.dao;
 import com.depromeet.stonebed.domain.missionRecord.domain.MissionRecordBoost;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
