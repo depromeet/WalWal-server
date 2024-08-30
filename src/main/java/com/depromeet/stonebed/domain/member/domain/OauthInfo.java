6b412161b4199e3d8e5eb90be32df4fabf45dfb8
ybchar <uiurihappy@sju.ac.kr>
2024-08-30 10:45:37 +0900

    fix: oauth_email unique 제약 조건 삭제

--- src/main/java/com/depromeet/stonebed/domain/member/domain/OauthInfo.java
+++ src/main/java/com/depromeet/stonebed/domain/member/domain/OauthInfo.java
@@ -1,7 +1,6 @@
 package com.depromeet.stonebed.domain.member.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -20,7 +19,6 @@ public class OauthInfo {
     private String oauthProvider;
 
     @Schema(description = "소셜 이메일", example = "test@gmail.com")
-    @Column(unique = true)
     private String oauthEmail;
 
     @Builder(access = AccessLevel.PRIVATE)
