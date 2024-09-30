3954f2963a6505b6c3fc2eed07b574413cd6d549
Park Yun Chan <dbscks9793@gmail.com>
2024-09-30 14:58:27 +0900

    fix: DTO에 Swagger Schema 어노테이션추가

--- src/main/java/com/depromeet/stonebed/domain/report/dto/response/ReportReasonResponse.java
+++ src/main/java/com/depromeet/stonebed/domain/report/dto/response/ReportReasonResponse.java
@@ -1,3 +1,8 @@
 package com.depromeet.stonebed.domain.report.dto.response;
 
-public record ReportReasonResponse(String enumValue, String description) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "신고 사유 응답 정보")
+public record ReportReasonResponse(
+        @Schema(description = "신고 사유 ENUM 값", example = "HARASSMENT") String enumValue,
+        @Schema(description = "신고 사유 설명", example = "사기 또는 사칭") String description) {}
